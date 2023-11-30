4e3bcf2c7cc51baac87b54840254877e06362358
Oleg Vologin <146234675+liberatd@users.noreply.github.com>
2023-11-30 23:26:54 +0400

    Update my commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        boolean test1 = bean == bean1;
+        boolean test2 = bean2 == bean3;
+        System.out.println("Результат пары бинов HelloWorld: " + test1);
+        System.out.println("Результат пары бинов Cat: " + test2);
     }
 }
